b70d3c92c7a42c7a3aca3b370324da4c242ac45b
alejoMaximo <alejomaximo1990@gmail.com>
2022-09-18 18:49:36 -0500

    agrege ccs html js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
 // agregando las variables del html
-const boton = document.getElementById('boton')
-const almacenaTexto = document.getElementById('agregaTexto')
+//const boton = document.getElementById('boton')
+//const almacenaTexto = document.getElementById('agregaTexto')
 
 // aqui se crea el evento click y la funcion a llamar
-boton.addEventListener('click', agregar)
+//boton.addEventListener('click', agregar)
 
 // se crea la funcion 'agregar'
-function agregar()
-{
+//function agregar()
+//{
     // llamamos el input almacenaTexto  .value es lo que se escribe
-    console.log(almacenaTexto.value)
-}
+   // console.log(almacenaTexto.value)
+
+//}
 
 //tarea imprimir estos datos en el documento
 
 //tarea : crea dos inputs y un boton  que sumen un valor
 
 // NOTA: hay que utilizar un poco mas de ingles
+
+function calcular() {
+    try {
+      const valor1 = parseFloat(document.getElementById('valor1').value) || 0,
+      const valor2 = parseFloat(document.getElementById('valor2').value) || 0,
+
+      document.getElementById("valortotal").value = valor1 * valor2;
+    
+    } catch (e) {}
+}

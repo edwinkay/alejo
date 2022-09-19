4ff04f2d8a540074dd40db578bf28f2ae7135924
alejoMaximo <alejomaximo1990@gmail.com>
2022-09-19 12:22:29 -0500

    agrege restas divicion y multiplicacion

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const total = document.getElementById('valortotal')
 // aqui se crea el evento click y la funcion a llamar
 boton.addEventListener('click', agregar)
 
+
+
 // se crea la funcion 'agregar'
 function agregar()
 {
@@ -38,3 +40,89 @@ function agregar()
     
 //     } catch (e) {}
 // }
+
+
+
+const boton2 = document.getElementById('boton2')
+const resta1 = document.getElementById('valor1')
+const resta2 = document.getElementById('valor2')
+const totalva = document.getElementById('valortotal')
+
+
+boton2.addEventListener('click', restar)
+
+
+
+
+function restar()
+{
+
+   const sum1 = resta1.value
+   const sum2 = resta2.value
+
+   let resultado
+
+   resultado = parseInt(sum1) - parseInt(sum2)
+
+
+
+   total.innerHTML = resultado
+   console.log(resultado)
+   
+}
+
+
+const boton3 = document.getElementById('boton3')
+const multiplicar1 = document.getElementById('valor1')
+const multiplicar2 = document.getElementById('valor2')
+const totalvar = document.getElementById('valortotal')
+
+
+boton3.addEventListener('click', multiplicar)
+
+
+
+
+function multiplicar()
+{
+
+   const sum1 = multiplicar1.value
+   const sum2 = multiplicar2.value
+
+   let resultado
+
+   resultado = parseInt(sum1) * parseInt(sum2)
+
+
+
+   total.innerHTML = resultado
+   console.log(resultado)
+}   
+
+
+const boton4 = document.getElementById('boton4')
+const dividir1 = document.getElementById('valor1')
+const dividir2 = document.getElementById('valor2')
+const totalvart = document.getElementById('valortotal')
+
+
+boton4.addEventListener('click', dividir)
+
+
+
+
+function dividir()
+{
+
+   const sum1 = dividir1.value
+   const sum2 = dividir2.value
+
+   let resultado
+
+   resultado = parseInt(sum1) / parseInt(sum2)
+
+
+
+   total.innerHTML = resultado
+   console.log(resultado)
+}   
